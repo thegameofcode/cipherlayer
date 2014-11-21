18396c1e8c9d1e959626ee252ee8ebf09e5fca23
Victor Fierro <victor.fierro@intelygenz.com>
2014-11-20 17:28:44 -0800

    minor fix

--- main.js
+++ main.js
@@ -9,6 +9,6 @@ cipherLayer.start(config.public_port, config.private_port, function(err){
     if(err){
         console.log('error on launch: ' + err);
     } else {
-        console.log('listening on port ' + config.listen_port);
+        console.log('listening on port ' + config.public_port);
     }
 });
