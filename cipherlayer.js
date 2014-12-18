d51241ec3adc6eb230f2d228313af181aef85f64
Luis Mesas <luismesas@gmail.com>
2014-12-18 12:31:23 -0800

    added timing of responses

--- cipherlayer.js
+++ cipherlayer.js
@@ -325,16 +325,18 @@ function startListener(publicPort, privatePort, cbk){
                 }
 
                 debug('=> '+ req.method + ' ' + options.url);
+                var start = Date.now();
                 request(options, function(err,private_res,body) {
+                    var timing = Date.now() - start;
                     if(err) {
-                        debug('<= ' + private_res.statusCode + ' ' + err);
+                        debug('<= ' + private_res.statusCode + ' ' + err + ' ' + timing + 'ms');
                         res.send(500, {err:'auth_proxy_error', des:'there was an internal error when redirecting the call to protected service'});
                     } else {
                         try{
                             body=JSON.parse(body);
-                            debug('<= ' + private_res.statusCode + ' json body');
+                            debug('<= ' + private_res.statusCode + ' json body' + ' ' + timing + 'ms');
                         } catch(err) {
-                            debug('<= ' + private_res.statusCode + ' no json body');
+                            debug('<= ' + private_res.statusCode + ' no json body' + ' ' + timing + 'ms');
                         }
                         res.send(Number(private_res.statusCode), body);
                     }
