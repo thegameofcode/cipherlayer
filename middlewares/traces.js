ba4d2cc4c68f4eae8d8c9f906346ef617f353e97
Kristina <kristina.lyubomirova@bbva.com>
2014-12-29 16:24:01 -0800

    var used when printing traces

--- middlewares/traces.js
+++ middlewares/traces.js
@@ -2,7 +2,7 @@ var debug = require('debug')('cipherlayer:service');
 var config = JSON.parse(require('fs').readFileSync('config.json','utf8'));
 
 function printTraces (req, res, next){
-    var url = 'http://localhost:' + config.private_port + req.url;
+    var url = req.options.url;
     debug('=> ' + req.method + ' ' + url);
     next();
 }
