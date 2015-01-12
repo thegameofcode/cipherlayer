e0c44e811368b188b8851670719e5a41feef6ebb
Victor Fierro <victor.fierro@intelygenz.com>
2015-01-12 10:45:37 -0800

    Minor change (uncaught exception)

--- cipherlayer.js
+++ cipherlayer.js
@@ -63,7 +63,7 @@ function startListener(publicPort, privatePort, cbk){
     server.on('uncaughtException', function(req, res, route, error) {
         var timing = Date.now() - new Date(req._time);
         debug('< ' + res.statusCode + ' ' + error + ' ' + timing + 'ms');
-        res.send(500, {err:'internal_error'});
+        res.send(500, {err:'internal_error', des: 'uncaught exception'});
     });
 
     var routesPath = path.join(__dirname, './routes/');
