14cbf9fa2b5c23ff6b67f692e8e0e86f6e20604d
gustavo.marin <gustavo.marin@intelygenz.com>
2015-10-13 13:49:04 +0200

    add support to transfer a list of allowedHeaders in the configuration

--- src/middlewares/propagateRequest.js
+++ src/middlewares/propagateRequest.js
@@ -73,6 +73,10 @@ function propagateRequest(req, res, next) {
 					},
 					user: req.user
 				}, 'proxy call');
+
+
+                transferAllowedHeaders(config.allowedHeaders, private_res, res);
+
 				if (private_res.statusCode === 302) {
 					res.header('Location', private_res.headers.location);
 					res.send(302);
@@ -84,6 +88,19 @@ function propagateRequest(req, res, next) {
 			return next();
 		});
 	}
+}
+
+function transferAllowedHeaders(headers, srcRes, dstRes) {
+
+    if (!headers || !headers.length ) {
+        return;
+    }
+
+    _.map(headers, function(header) {
+        if (srcRes.headers[header]) {
+            dstRes.header(header, srcRes.headers[header] );
+        }
+    });
 }
 
 module.exports = propagateRequest;
