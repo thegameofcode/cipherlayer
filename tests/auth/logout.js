a8d8629cc1ddfd45a34c2e377b2e7502b9ced712
Luis Mesas <luismesas@gmail.com>
2016-02-08 11:43:38 -0800

    fixed impersonation exploit on logout

--- tests/auth/logout.js
+++ tests/auth/logout.js
@@ -1,78 +1,82 @@
+'use strict';
+
 var assert = require('assert');
 var clone = require('clone');
 var request = require('request');
-var ciphertoken = require('ciphertoken');
 var config = require('../../config.json');
 var dao = require('../../src/managers/dao.js');
+var should = require('chai').should();
+var nock = require('nock');
 
-var cryptoMng = require('../../src/managers/crypto')({ password : 'password' });
+var cryptoMng = require('../../src/managers/crypto')({password: 'password'});
 
 module.exports = {
-    describe: function(accessTokenSettings, refreshTokenSettings){
-        describe('/login', function () {
-            var baseUser = {
-                id: 'a1b2c3d4e5f6',
-                username: 'validuser' + (config.allowedDomains && config.allowedDomains[0] ? config.allowedDomains[0].replace('*','') : ''),
-                password: 'validpassword',
-                deviceId: '1234567890'
-            };
+	describe: function (accessTokenSettings) {
+		describe('/logout', function () {
+			var baseUser = {
+				id: 'a1b2c3d4e5f6',
+				username: 'validuser' + (config.allowedDomains && config.allowedDomains[0] ? config.allowedDomains[0].replace('*', '') : ''),
+				password: 'validpassword',
+				deviceId: '1234567890'
+			};
 
-            beforeEach(function (done) {
-                dao.deleteAllUsers(function (err) {
-                    assert.equal(err, null);
-                    var userToCreate = clone(baseUser);
-                    cryptoMng.encrypt(userToCreate.password, function(encryptedPwd) {
-                        userToCreate.password = encryptedPwd;
-                        dao.addUser()(userToCreate, function (err, createdUser) {
-                            assert.equal(err, null);
-                            assert.notEqual(createdUser, undefined);
-                            done();
-                        });
-                    });
-                });
-            });
-            it('POST 204', function (done) {
-                var user = clone(baseUser);
-                var options = {
-                    url: 'http://localhost:' + config.public_port + '/auth/login',
-                    headers: {
-                        'Content-Type': 'application/json; charset=utf-8'
-                    },
-                    method: 'POST',
-                    body: JSON.stringify(user)
-                };
-                options.headers[config.version.header] = "test/1";
+			beforeEach(function (done) {
+				dao.deleteAllUsers(function (err) {
+					assert.equal(err, null);
+					var userToCreate = clone(baseUser);
+					cryptoMng.encrypt(userToCreate.password, function (encryptedPwd) {
+						userToCreate.password = encryptedPwd;
+						dao.addUser()(userToCreate, function (err, createdUser) {
+							assert.equal(err, null);
+							assert.notEqual(createdUser, undefined);
+							done();
+						});
+					});
+				});
+			});
 
-                request(options, function (err, res, body) {
-                    assert.equal(err, null);
-                    assert.equal(res.statusCode, 200, body);
-                    body = JSON.parse(body);
-                    assert.notEqual(body.accessToken, undefined);
-                    assert.equal(body.expiresIn, accessTokenSettings.tokenExpirationMinutes);
+			function doLogin() {
+				return new Promise(ok => {
+					var user = clone(baseUser);
+					var options = {
+						url: 'http://localhost:' + config.public_port + '/auth/login',
+						headers: {},
+						method: 'POST',
+						body: user,
+						json: true
+					};
+					options.headers[config.version.header] = "test/1";
 
-                    ciphertoken.getTokenSet(accessTokenSettings, body.accessToken, function (err, accessTokenInfo) {
-                        assert.equal(err, null);
-                        assert.equal(accessTokenInfo.userId, user.id);
-                        assert.equal(accessTokenInfo.data.deviceId, user.deviceId);
-                        assert.notEqual(body.refreshToken, undefined);
+					request(options, function (err, res, body) {
+						should.not.exist(err);
+						res.statusCode.should.equal(200);
+						body.should.have.property('accessToken');
+						body.expiresIn.should.equal(accessTokenSettings.tokenExpirationMinutes);
+						ok(body.accessToken);
+					});
+				});
+			}
 
-                        ciphertoken.getTokenSet(refreshTokenSettings, body.refreshToken, function (err, refreshTokenInfo) {
-                            assert.equal(err, null);
-                            assert.equal(refreshTokenInfo.userId, user.id);
-                            assert.equal(accessTokenInfo.data.deviceId, user.deviceId);
+			it('POST 204', function (done) {
+				doLogin().then(accessToken => {
+					var options = {
+						url: 'http://localhost:' + config.public_port + '/auth/logout',
+						method: 'POST',
+						headers: {
+							'Authorization': 'bearer ' + accessToken
+						}
+					};
+					options.headers[config.version.header] = "test/1";
 
-                            options.url = 'http://localhost:' + config.public_port + '/auth/login';
-                            options.body({"userId":user.id, "deviceId": user.deviceId});
-                            request(options, function(err, res, body){
-                                assert.equal(err, null);
-                                assert.equal(res.statusCode, 204, body);
-                                done();
-                            });
+					nock('http://' + config.private_host + ':' + config.private_port).delete('/api/me/session').reply(200);
 
-                        });
-                    });
-                });
-            });
-        });
-    }
+					request(options, function (err, res, body) {
+						should.not.exist(err);
+						res.statusCode.should.equal(204, body);
+						done();
+					});
+				});
+			});
+		});
+	}
 };
