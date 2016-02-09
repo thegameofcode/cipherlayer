4b28ea92b5c9b52749952fc60837af7539f7b848
Luis Mesas <luismesas@gmail.com>
2016-02-08 18:21:12 -0800

    improved redis tests

--- src/managers/redis.js
+++ src/managers/redis.js
@@ -5,104 +5,108 @@ var redisClient;
 
 var isConnected;
 
-function connect(cbk){
-    var host = config.redis.host || "localhost";
-    var port = config.redis.port || 6379;
+function connect(cbk) {
+	var host = config.redis.host || "localhost";
+	var port = config.redis.port || 6379;
 
-    redisClient = redis.createClient(port, host, {});
-    redisClient.on('connect', function (err) {
-        if(err) return cbk(err);
-        isConnected = true;
-        cbk(null, true);
-    });
+	redisClient = redis.createClient(port, host, {});
+	redisClient.on('connect', function (err) {
+		if (err) return cbk(err);
+		isConnected = true;
+		cbk(null, true);
+	});
 }
 
-function disconnect(cbk){
-    redisClient.end();
-    isConnected = false;
-    cbk(null, false);
+function disconnect(cbk) {
+	if (!isConnected) {
+		return cbk();
+	}
+
+	redisClient.end();
+	isConnected = false;
+	cbk(null, false);
 }
 
-function insertKeyValue(key, value, expSeconds, cbk){
-    if(!redisClient){
-        return cbk({err:'redis_not_connected'});
-    }
+function insertKeyValue(key, value, expSeconds, cbk) {
+	if (!isConnected || !redisClient) {
+		return cbk({err: 'redis_not_connected'});
+	}
 
-    redisClient.set(key, value, function(err) {
-        if (err) {
-            return cbk(err);
-        } else {
-            redisClient.get(key, function(err, value) {
-                if (err) {
-                    return cbk(err);
-                } else {
-                    if(expSeconds){
-                        redisClient.expire(key, expSeconds);
-                    }
-                    return cbk(null,value);
-                }
-            });
-        }
-    });
+	redisClient.set(key, value, function (err) {
+		if (err) {
+			return cbk(err);
+		} else {
+			redisClient.get(key, function (err, value) {
+				if (err) {
+					return cbk(err);
+				} else {
+					if (expSeconds) {
+						redisClient.expire(key, expSeconds);
+					}
+					return cbk(null, value);
+				}
+			});
+		}
+	});
 }
 
-function updateKeyValue(key, value, cbk){
-    if(!redisClient){
-        return cbk({err:'redis_not_connected'});
-    }
+function updateKeyValue(key, value, cbk) {
+	if (!isConnected || !redisClient) {
+		return cbk({err: 'redis_not_connected'});
+	}
 
-    redisClient.ttl(key, function(err, expSeconds){
-        if (err) return cbk(err);
-        insertKeyValue(key, value, expSeconds, cbk);
-    });
+	redisClient.ttl(key, function (err, expSeconds) {
+		if (err) return cbk(err);
+		insertKeyValue(key, value, expSeconds, cbk);
+	});
 }
 
-function getKeyValue(key, cbk){
-    if(!redisClient){
-        return cbk({err:'redis_not_connected'});
-    }
+function getKeyValue(key, cbk) {
+	if (!isConnected || !redisClient) {
+		return cbk({err: 'redis_not_connected'});
+	}
 
-    redisClient.get(key, function (err, value) {
-        return cbk(err, value);
-    });
+	redisClient.get(key, function (err, value) {
+		return cbk(err, value);
+	});
 }
 
-function deleteKeyValue(key, cbk){
-    if(!redisClient){
-        return cbk({err:'redis_not_connected'});
-    }
+function deleteKeyValue(key, cbk) {
+	if (!isConnected || !redisClient) {
+		return cbk({err: 'redis_not_connected'});
+	}
 
-    redisClient.del(key, function (err, deleted) {
-        return cbk(err, (deleted == 1));
-    });
+	redisClient.del(key, function (err, deleted) {
+		return cbk(err, (deleted == 1));
+	});
 }
 
-function deleteAllKeys(cbk){
-    if(!redisClient){
-        return cbk({err:'redis_not_connected'});
-    }
+function deleteAllKeys(cbk) {
+	if (!isConnected || !redisClient) {
+		return cbk({err: 'redis_not_connected'});
+	}
 
-    redisClient.flushall(cbk);
+	redisClient.flushall(cbk);
 }
 
-function getStatus(cbk){
-    var REDIS_ERR = {
-        err: 'component_error',
-        des: 'Redis component is not available'
-    };
+function getStatus(cbk) {
+	var REDIS_ERR = {
+		err: 'component_error',
+		des: 'Redis component is not available'
+	};
 
-    if(!redisClient || !isConnected) return cbk(REDIS_ERR);
-    cbk();
+	if (!redisClient || !isConnected) return cbk(REDIS_ERR);
+	cbk();
 }
 
 module.exports = {
-    connect: connect,
-    disconnect: disconnect,
-    insertKeyValue: insertKeyValue,
-    updateKeyValue: updateKeyValue,
-    getKeyValue: getKeyValue,
-    deleteKeyValue: deleteKeyValue,
-    deleteAllKeys: deleteAllKeys,
+	connect: connect,
+	disconnect: disconnect,
+	insertKeyValue: insertKeyValue,
+	updateKeyValue: updateKeyValue,
+	getKeyValue: getKeyValue,
+	deleteKeyValue: deleteKeyValue,
+	deleteAllKeys: deleteAllKeys,
 
-    getStatus: getStatus
+	getStatus: getStatus
 };
--- tests/redis.js
+++ tests/redis.js
@@ -1,129 +1,211 @@
+'use strict';
+
 var redisMng = require('../src/managers/redis');
 var assert = require('assert');
 var async = require('async');
 
-describe('redis', function() {
-    beforeEach(function (done) {
-        redisMng.connect(done);
-    });
+describe('redis', function () {
+	beforeEach(function (done) {
+		redisMng.connect(done);
+	});
 
-    afterEach(function (done) {
-        redisMng.disconnect(done);
-    });
+	afterEach(function (done) {
+		redisMng.disconnect(done);
+	});
 
-    var baseKey = 'key';
-    var baseValue = 'value';
+	var baseKey = 'key';
+	var baseValue = 'value';
 
-    it('insert', function (done) {
-        redisMng.insertKeyValue(baseKey, baseValue, 3, function(err){
-            assert.equal(err, null);
-            done();
-        });
-    });
+	it('insert', function (done) {
+		redisMng.insertKeyValue(baseKey, baseValue, 3, function (err) {
+			assert.equal(err, null);
+			done();
+		});
+	});
 
-    it('get', function (done) {
-        redisMng.getKeyValue(baseKey, function(err, value){
-            assert.equal(err, null);
-            assert.equal(value, baseValue);
-            done();
-        });
-    });
+	it('insert - disconnected', function (done) {
+		redisMng.disconnect(function () {
+			redisMng.insertKeyValue(baseKey, baseValue, 3, function (err) {
+				assert.deepEqual(err, {err: 'redis_not_connected'});
+				done();
+			});
+		});
+	});
 
-    it('delete', function (done) {
-        redisMng.deleteKeyValue(baseKey, function(err, deleted){
-            assert.equal(err, null);
-            assert.equal(deleted, true);
-            redisMng.getKeyValue(baseKey, function(err, value){
-                assert.equal(err, null);
-                assert.equal(value, null);
-                done();
-            });
-        });
-    });
+	it('get', function (done) {
+		redisMng.getKeyValue(baseKey, function (err, value) {
+			assert.equal(err, null);
+			assert.equal(value, baseValue);
+			done();
+		});
+	});
 
-    it('expire', function (done) {
-        this.timeout(4000);
-        async.series([
-            createKey = function(done){
-                redisMng.insertKeyValue(baseKey, baseValue, 2, function(err){
-                    assert.equal(err, null);
-                    done();
-                });
-            },
-            checkExpire = function(done){
-                setTimeout(
-                    function () {
-                        redisMng.getKeyValue(baseKey, function (err, value) {
-                            assert.equal(err, null);
-                            assert.equal(value, null);
-                            done();
-                        });
-                    }, 2500
-                );
-            }
-        ], done);
-    });
+	it('get - disconnected', function (done) {
+		redisMng.disconnect(function () {
+			redisMng.getKeyValue(baseKey, function (err, value) {
+				assert.deepEqual(err, {err: 'redis_not_connected'});
+				done();
+			});
+		});
+	});
 
-    it('update', function (done) {
-        this.timeout(4000);
-        var val = 'new value';
-        async.series([
-            createKey = function(done){
-                redisMng.insertKeyValue(baseKey, baseValue, 3, function(err){
-                    assert.equal(err, null);
-                    done();
-                });
-            },
-            updateKey = function(done){
-                setTimeout(
-                    function () {
-                        redisMng.updateKeyValue(baseKey, val, function (err) {
-                            assert.equal(err, null);
-                            redisMng.getKeyValue(baseKey, function (err, value) {
-                                assert.equal(err, null);
-                                assert.equal(value, val);
-                                done();
-                            });
-                        });
-                    } , 2000
-                );
-            },
-            checkExpire = function(done){
-                setTimeout(
-                    function () {
-                        redisMng.getKeyValue(baseKey, function (err, value) {
-                            assert.equal(err, null);
-                            assert.equal(value, null);
-                            done();
-                        });
-                    }, 1500
-                );
-            }
-        ], done);
-    });
+	it('delete', function (done) {
+		redisMng.deleteKeyValue(baseKey, function (err, deleted) {
+			assert.equal(err, null);
+			assert.equal(deleted, true);
+			redisMng.getKeyValue(baseKey, function (err, value) {
+				assert.equal(err, null);
+				assert.equal(value, null);
+				done();
+			});
+		});
+	});
 
-    it('delete all', function (done) {
-        async.series([
-            createKey = function(done){
-                redisMng.insertKeyValue(baseKey, baseValue, 10, function(err){
-                    assert.equal(err, null);
-                    done();
-                });
-            },
-            deleteAllKeys = function(done){
-                redisMng.deleteAllKeys(function (err) {
-                    assert.equal(err, null);
-                    done();
-                });
-            },
-            checkDelete = function(done){
-                redisMng.getKeyValue(baseKey, function (err, value) {
-                    assert.equal(err, null);
-                    assert.equal(value, null);
-                    done();
-                });
-            }
+	it('delete - disconnected', function (done) {
+		redisMng.disconnect(function () {
+			redisMng.deleteKeyValue(baseKey, function (err) {
+				assert.deepEqual(err, {err: 'redis_not_connected'});
+				done();
+			});
+		});
+	});
 
-        ], done);
-    });
-});
+	it('expire', function (done) {
+		this.timeout(4000);
+		async.series([
+			//createKey
+			function (done) {
+				redisMng.insertKeyValue(baseKey, baseValue, 2, function (err) {
+					assert.equal(err, null);
+					done();
+				});
+			},
+			// checkExpire
+			function (done) {
+				setTimeout(
+					function () {
+						redisMng.getKeyValue(baseKey, function (err, value) {
+							assert.equal(err, null);
+							assert.equal(value, null);
+							done();
+						});
+					}, 2500
+				);
+			}
+		], done);
+	});
+
+	it('update', function (done) {
+		this.timeout(4000);
+		var val = 'new value';
+		async.series([
+			// createKey
+			function (done) {
+				redisMng.insertKeyValue(baseKey, baseValue, 3, function (err) {
+					assert.equal(err, null);
+					done();
+				});
+			},
+			// updateKey
+			function (done) {
+				setTimeout(
+					function () {
+						redisMng.updateKeyValue(baseKey, val, function (err) {
+							assert.equal(err, null);
+							redisMng.getKeyValue(baseKey, function (err, value) {
+								assert.equal(err, null);
+								assert.equal(value, val);
+								done();
+							});
+						});
+					}, 2000
+				);
+			},
+			// checkExpire
+			function (done) {
+				setTimeout(
+					function () {
+						redisMng.getKeyValue(baseKey, function (err, value) {
+							assert.equal(err, null);
+							assert.equal(value, null);
+							done();
+						});
+					}, 1500
+				);
+			}
+		], done);
+	});
+
+	it('update - disconnected', function (done) {
+		this.timeout(4000);
+		var val = 'new value';
+		async.series([
+			// createKey
+			function (done) {
+				redisMng.insertKeyValue(baseKey, baseValue, 3, function (err) {
+					assert.equal(err, null);
+					done();
+				});
+			},
+			// updateKey
+			function (done) {
+				redisMng.disconnect(function () {
+					redisMng.updateKeyValue(baseKey, val, function (err) {
+						assert.deepEqual(err, {err: 'redis_not_connected'});
+						done();
+					});
+				});
+			}
+		], done);
+	});
+
+	it('delete all', function (done) {
+		async.series([
+			// createKey
+			function (done) {
+				redisMng.insertKeyValue(baseKey, baseValue, 10, function (err) {
+					assert.equal(err, null);
+					done();
+				});
+			},
+			// deleteAllKeys
+			function (done) {
+				redisMng.deleteAllKeys(function (err) {
+					assert.equal(err, null);
+					done();
+				});
+			},
+			// checkDelete
+			function (done) {
+				redisMng.getKeyValue(baseKey, function (err, value) {
+					assert.equal(err, null);
+					assert.equal(value, null);
+					done();
+				});
+			}
+		], done);
+	});
+
+	it('delete all -- disconnected', function (done) {
+		async.series([
+			// createKey
+			function (done) {
+				redisMng.insertKeyValue(baseKey, baseValue, 10, function (err) {
+					assert.equal(err, null);
+					done();
+				});
+			},
+			// deleteAllKeys
+			function (done) {
+				redisMng.disconnect(function () {
+					redisMng.deleteAllKeys(function (err) {
+						assert.deepEqual(err, {err: 'redis_not_connected'});
+						done();
+					});
+				});
+			}
+		], done);
+	});
+
+})
+;
