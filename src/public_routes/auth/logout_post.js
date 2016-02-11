966b909c6e9da391707b08940c7b77240ea470d7
Luis Mesas <luismesas@gmail.com>
2016-02-11 08:16:34 -0800

    drop error to log when decoding token on logout

--- src/public_routes/auth/logout_post.js
+++ src/public_routes/auth/logout_post.js
@@ -22,7 +22,7 @@ module.exports = function (req, res, next) {
 
 	tokenMng.getAccessTokenInfo(accessToken, function (err, tokenInfo) {
 		if (err) {
-			log.error({err: 'invalid_access_token', des: accessToken});
+			log.error({err: 'invalid_access_token', des: err});
 			res.send(401, {err: 'invalid_access_token', des: 'unable to read token info'});
 			return next(false);
 		} else {
