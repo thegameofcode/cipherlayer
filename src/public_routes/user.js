78d89a1dc1455407e78db9c2876610c39cd7dd7a
Luis Mesas <luismesas@gmail.com>
2016-02-23 15:17:33 -0800

    extracted user activation to a single module

--- src/public_routes/user.js
+++ src/public_routes/user.js
@@ -12,6 +12,8 @@ var findUser = require('../middlewares/findUser');
 var _ = require('lodash');
 
 var forgotPassword_get = require('./user/forgotPassword_get');
+var activateUser_get = require('./user/activateUser_get');
+var activateUser_post = require('./user/activateUser_get');
 
 function createUserEndpoint(req, res, next) {
 	userMng().createUser(req.body, req.headers['x-otp-pin'], function (error, tokens) {
@@ -44,67 +46,6 @@ function createUserEndpoint(req, res, next) {
 			});
 		});
 
-	});
-}
-
-function createUserByToken(req, res, next) {
-	if (!req.params) {
-		res.send(400, {
-			err: 'invalid_url_params',
-			des: 'The call to this url must have params.'
-		});
-		return next();
-	}
-
-	userMng().createUserByToken(req.params.verifyToken, function (err, tokens) {
-		if (err) {
-			if (!err.code) {
-				res.send(500, err);
-			} else {
-				var errCode = err.code;
-				delete(err.code);
-				res.send(errCode, err);
-			}
-			return next(false);
-		} else {
-
-			if (req.method === 'POST') {
-				res.send(200, tokens);
-				return next();
-			}
-
-			var compatibleDevices = config.emailVerification.compatibleEmailDevices;
-			var userAgent = String(req.headers['user-agent']);
-
-			for (var i = 0; i < compatibleDevices.length; i++) {
-				var exp = compatibleDevices[i];
-				var check = exp.replace(/\*/g, '.*');
-				var match = userAgent.match(check);
-				var isCompatible = (match !== null && userAgent === match[0]);
-				if (isCompatible) {
-					match = userAgent.match(/.*Android.*/i);
-					var isAndroid = (match !== null && userAgent === match[0]);
-					var location = config.emailVerification.scheme + '://user/refreshToken/' + tokens.refreshToken;
-
-					if (isAndroid) {
-						location = 'intent://user/refreshToken/' + tokens.refreshToken + '/#Intent;scheme=' + config.emailVerification.scheme + ';end';
-					}
-					res.header('Location', location);
-					res.send(302);
-					return next(false);
-				}
-			}
-
-			if (config.emailVerification.redirectUrl) {
-				var refreshToken = config.emailVerification.redirectRefreshToken ? '?refreshToken=' + tokens.refreshToken : '';
-				res.setHeader('Location', config.emailVerification.redirectUrl + refreshToken);
-				res.send(301);
-				return next();
-			}
-
-			res.send(200, {msg: config.emailVerification.nonCompatibleEmailMsg});
-			return next();
-		}
 	});
 }
 
@@ -191,8 +132,8 @@ function addRoutes(service) {
 	service.get('/user/:email/password', forgotPassword_get);
 
 	service.post(config.passThroughEndpoint.path, createUserEndpoint);
-	service.get('/user/activate', createUserByToken);
-	service.post('/user/activate', createUserByToken);
+	service.get('/user/activate', activateUser_get);
+	service.post('/user/activate', activateUser_post);
 	service.post('/user/email/available', checkEmailAvailable);
 	service.put('/user/me/password', checkAccessTokenParam, checkAuthHeader, decodeToken, requireBody, findUser, validateOldPassword, setPassword);
 }
