47357b93e188f3d55035ba57a328274ada66b58d
Luis Mesas <luismesas@gmail.com>
2016-02-23 12:19:58 -0800

    removed check no longer needed

--- src/public_routes/auth/logout_post.js
+++ src/public_routes/auth/logout_post.js
@@ -4,18 +4,10 @@ var sessionRequest = require('./session');
 var log = require('../../logger/service.js');
 
 module.exports = function (req, res, next) {
-	var authHeader = req.header('Authorization');
-	if (!authHeader) {
-		log.error({err: 'invalid_access_token', des: 'no authorization header'});
-		res.send(401, {err: 'invalid_access_token', des: 'unable to read token info'});
-		return next(false);
-	}
-
 	var tokenInfo = req.tokenInfo;
-
-	var userAgent = String(req.headers['user-agent']);
 	var userId = tokenInfo.userId;
 	var deviceId = tokenInfo.data.deviceId;
+	var userAgent = String(req.headers['user-agent']);
 
 	sessionRequest(deviceId, userId, 'DELETE', userAgent, function (err, result) {
 		if (err) {
