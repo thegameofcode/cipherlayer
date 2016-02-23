d813a4a6c3a468ef634906a6cb5ce5b33384a40e
Luis Mesas <luismesas@gmail.com>
2016-02-23 15:36:21 -0800

    extracted user endpoints to independent modules

--- src/public_routes/user.js
+++ src/public_routes/user.js
@@ -1,53 +1,18 @@
 'use strict';
 
-var daoMng = require('../managers/dao');
 var config = require(process.cwd() + '/config.json');
-var tokenMng = require('../managers/token');
 var userMng = require('../managers/user');
 
 var checkAccessTokenParam = require('../middlewares/accessTokenParam');
 var checkAuthHeader = require('../middlewares/authHeaderRequired');
 var decodeToken = require('../middlewares/decodeToken');
 var findUser = require('../middlewares/findUser');
-var _ = require('lodash');
 
 var forgotPassword_get = require('./user/forgotPassword_get');
 var activateUser_get = require('./user/activateUser_get');
 var activateUser_post = require('./user/activateUser_get');
-
-function createUserEndpoint(req, res, next) {
-	userMng().createUser(req.body, req.headers['x-otp-pin'], function (error, tokens) {
-		if (error) {
-			if (!error.code) {
-				res.send(500, error);
-				return next(false);
-			}
-			var errCode = error.code;
-			res.send(errCode, {err: error.err, des: error.des});
-			return next(false);
-		}
-
-		tokenMng.getRefreshTokenInfo(tokens.refreshToken, function (err, tokenSet) {
-			if (err) {
-				res.send(500, {err: 'internal_error', des: 'error creating user tokens'});
-				return next(false);
-			}
-
-			var userId = tokenSet.userId;
-			var tokenData = tokenSet.data;
-
-			if (config.version) {
-				tokenData.deviceVersion = req.headers[config.version.header];
-			}
-
-			tokenMng.createBothTokens(userId, tokenData, function (err, tokens) {
-				res.send(201, tokens);
-				return next();
-			});
-		});
-
-	});
-}
+var checkEmailAvailability_post = require('./user/checkEmailAvailability_post');
+var createUser_post = require('./user/createUser_post');
 
 function requireBody(req, res, next) {
 	var err;
@@ -106,35 +71,12 @@ function setPassword(req, res, next) {
 	});
 }
 
-function checkEmailAvailable(req, res, next) {
-	var email = req.body.email;
-
-	if (_.isEmpty(email)) {
-		res.send(400, {
-			err: 'BadRequestError',
-			des: 'Missing email in request body'
-		});
-		return next(false);
-	}
-
-	daoMng.findByEmail(email, function (error, output) {
-		if (error) {
-			res.send(error.statusCode, error.body);
-			return next(false);
-		}
-
-		res.send(200, output);
-		return next();
-	});
-}
-
 function addRoutes(service) {
 	service.get('/user/:email/password', forgotPassword_get);
-
-	service.post(config.passThroughEndpoint.path, createUserEndpoint);
+	service.post(config.passThroughEndpoint.path, createUser_post);
 	service.get('/user/activate', activateUser_get);
 	service.post('/user/activate', activateUser_post);
-	service.post('/user/email/available', checkEmailAvailable);
+	service.post('/user/email/available', checkEmailAvailability_post);
 	service.put('/user/me/password', checkAccessTokenParam, checkAuthHeader, decodeToken, requireBody, findUser, validateOldPassword, setPassword);
 }
 
---
+++ src/public_routes/user/createUser_post.js
@@ -0,0 +1,39 @@
+'use strict';
+
+var config = require(process.cwd() + '/config.json');
+var tokenMng = require('../../managers/token');
+var userMng = require('../../managers/user');
+
+module.exports = function (req, res, next) {
+	userMng().createUser(req.body, req.headers['x-otp-pin'], function (error, tokens) {
+		if (error) {
+			if (!error.code) {
+				res.send(500, error);
+				return next(false);
+			}
+			var errCode = error.code;
+			res.send(errCode, {err: error.err, des: error.des});
+			return next(false);
+		}
+
+		tokenMng.getRefreshTokenInfo(tokens.refreshToken, function (err, tokenSet) {
+			if (err) {
+				res.send(500, {err: 'internal_error', des: 'error creating user tokens'});
+				return next(false);
+			}
+
+			var userId = tokenSet.userId;
+			var tokenData = tokenSet.data;
+
+			if (config.version) {
+				tokenData.deviceVersion = req.headers[config.version.header];
+			}
+
+			tokenMng.createBothTokens(userId, tokenData, function (err, tokens) {
+				res.send(201, tokens);
+				return next();
+			});
+		});
+
+	});
+};
