ca4f2b0ef1f820da8943d21f862eb053b4fafd91
Luis Mesas <luismesas@gmail.com>
2016-02-23 15:11:35 -0800

    extracted forgot password to a single module

--- src/public_routes/user.js
+++ src/public_routes/user.js
@@ -1,10 +1,7 @@
-var async = require('async');
+'use strict';
 
 var daoMng = require('../managers/dao');
 var config = require(process.cwd() + '/config.json');
-var crypto = require('../managers/crypto');
-var cryptoMng = crypto(config.password);
-var emailMng = require('../managers/email');
 var tokenMng = require('../managers/token');
 var userMng = require('../managers/user');
 
@@ -13,111 +10,8 @@ var checkAuthHeader = require('../middlewares/authHeaderRequired');
 var decodeToken = require('../middlewares/decodeToken');
 var findUser = require('../middlewares/findUser');
 var _ = require('lodash');
-var log = require('../logger/service.js');
 
-function sendNewPassword(req, res, next) {
-	if (!req.params.email) {
-		res.send(400, {
-			err: 'auth_proxy_error',
-			des: 'empty email'
-		});
-		return next(false);
-	}
-
-	daoMng.getAllUserFields(req.params.email, function (err, foundUser) {
-		if (!foundUser) {
-			res.send(404, {
-				err: 'user_not_found',
-				des: 'email does not exists'
-			});
-			return next(false);
-		}
-		var passwd = cryptoMng.randomPassword(config.password.regexValidation);
-
-		cryptoMng.encrypt(passwd, function (encryptedPassword) {
-			var fieldValue = [];
-
-			if (Array.isArray(foundUser.password)) {
-				fieldValue = [foundUser.password[0], encryptedPassword];
-			} else {
-				fieldValue = [foundUser.password, encryptedPassword];
-			}
-
-			daoMng.updateField(foundUser._id, 'password', fieldValue, function (err) {
-				if (err) {
-					res.send(500, {
-						err: 'auth_proxy_error',
-						des: 'internal error setting a new password'
-					});
-
-					return next(false);
-
-				} else {
-					var data = {};
-					if (foundUser.roles) {
-						data.roles = foundUser.roles;
-					}
-
-					async.series([
-						function (done) {
-							//Add "realms" & "capabilities"
-							daoMng.getRealms(function (err, realms) {
-								if (err) {
-									log.error({err: err, des: 'error obtaining user realms'});
-									return done();
-								}
-
-								if (!realms) {
-									log.info({des: 'there are no REALMS in DB'});
-									return done();
-								}
-								async.eachSeries(realms, function (realm, next) {
-									if (!realm.allowedDomains || !realm.allowedDomains.length) {
-										return next();
-									}
-									async.eachSeries(realm.allowedDomains, function (domain, more) {
-										//wildcard
-										var check = domain.replace(/\*/g, '.*');
-										var match = foundUser.username.match(check);
-										if (!match || foundUser.username !== match[0]) {
-											return more();
-										}
-
-										if (!data.realms) {
-											data.realms = [];
-										}
-										data.realms.push(realm.name);
-
-										async.each(Object.keys(realm.capabilities), function (capName, added) {
-											if (!data.capabilities) {
-												data.capabilities = {};
-											}
-
-											data.capabilities[capName] = realm.capabilities[capName];
-											added();
-										}, more);
-									}, next);
-								}, done);
-							});
-						}
-					], function () {
-						tokenMng.createBothTokens(foundUser._id, data, function (err, tokens) {
-							var link = config.emailVerification.redirectProtocol + '://user/refreshToken/' + tokens.refreshToken;
-							emailMng().sendEmailForgotPassword(req.params.email, passwd, link, function (err) {
-								if (err) {
-									res.send(500, {err: 'internalError', des: 'Internal server error'});
-								} else {
-									res.send(204);
-								}
-								return next(false);
-							});
-						});
-					});
-				}
-			});
-		});
-	});
-}
+var forgotPassword_get = require('./user/forgotPassword_get');
 
 function createUserEndpoint(req, res, next) {
 	userMng().createUser(req.body, req.headers['x-otp-pin'], function (error, tokens) {
@@ -294,7 +188,7 @@ function checkEmailAvailable(req, res, next) {
 }
 
 function addRoutes(service) {
-	service.get('/user/:email/password', sendNewPassword);
+	service.get('/user/:email/password', forgotPassword_get);
 
 	service.post(config.passThroughEndpoint.path, createUserEndpoint);
 	service.get('/user/activate', createUserByToken);
