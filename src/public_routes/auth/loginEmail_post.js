818085fcfd8e4d841148c38d73ccbcb3170823ae
Luis Mesas <luismesas@gmail.com>
2016-02-24 17:32:36 -0800

    added magic link endpoint

---
+++ src/public_routes/auth/loginEmail_post.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var emailManager = require('../../managers/email')();
+var config = require(process.cwd() + '/config.json');
+var tokenManager = require('../../managers/token');
+var daoManager = require('../../managers/dao');
+
+module.exports = function (req, res, next) {
+
+	var email = req.params.email;
+
+	if (!email) {
+		res.send(400, {
+			err: 'invalid_email',
+			des: 'email is required'
+		});
+		return next(false);
+	}
+
+	daoManager.getFromUsername(email, function (err, user) {
+		tokenManager.createRefreshToken(user._id, {}, function (err, refreshToken) {
+			var link = config.emailVerification.redirectProtocol + '://auth/login/refreshToken/' + refreshToken;
+			emailManager.sendEmailMagicLink(email, link, function () {
+				res.send(204);
+				next();
+			});
+		});
+	});
+
+};
--- tests/managers/email.js
+++ tests/managers/email.js
@@ -22,12 +22,63 @@ describe('managers', function () {
 				};
 
 				nock(config.externalServices.notifications.base)
-					.log(console.log)
 					.post(config.externalServices.notifications.pathEmail, nockBody)
 					.reply(200, {});
 
 				emailMng.sendEmailForgotPassword(email, password, link, function (err) {
 					should.not.exist(err);
+					done();
+				});
+			});
+		});
+
+		describe('magic link', function () {
+			it('OK', function (done) {
+				var expectedEmail = 'user@email.com';
+				var expectedLink = 'http://magic_link';
+
+				var html = config.magicLink.body.replace("__LINK__", expectedLink);
+
+				var nockBody = {
+					to: expectedEmail,
+					subject: config.magicLink.subject,
+					html: html
+				};
+
+				var nockedEndpoint = nock(config.externalServices.notifications.base)
+					.post(config.externalServices.notifications.pathEmail, nockBody)
+					.reply(200, {});
+
+				emailMng.sendEmailMagicLink(expectedEmail, expectedLink, function (err) {
+					should.not.exist(err);
+					nockedEndpoint.isDone().should.equal(true);
+					done();
+				});
+			});
+
+			it('FAIL - notifications service failed', function (done) {
+				var expectedEmail = 'user@email.com';
+				var expectedLink = 'http://magic_link';
+
+				var html = config.magicLink.body.replace("__LINK__", expectedLink);
+
+				var nockBody = {
+					to: expectedEmail,
+					subject: config.magicLink.subject,
+					html: html
+				};
+
+				var nockedEndpoint = nock(config.externalServices.notifications.base)
+					.post(config.externalServices.notifications.pathEmail, nockBody)
+					.reply(500, {});
+
+				emailMng.sendEmailMagicLink(expectedEmail, expectedLink, function (err) {
+					should.exist(err);
+					err.should.deep.equal({
+						err: 'internal_error',
+						des: 'Error calling notifications service for Magic Link email'
+					});
+					nockedEndpoint.isDone().should.equal(true);
 					done();
 				});
 			});
