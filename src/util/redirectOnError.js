ba481d42b57bb167d18f99a732fe92d6dd9e4258
gustavo.marin <gustavo.marin@intelygenz.com>
2016-05-12 18:27:18 +0200

    added no-cache header

--- src/util/redirectOnError.js
+++ src/util/redirectOnError.js
@@ -6,16 +6,20 @@ const logger = require('../logger/service');
 const redirectOnErrorEnabled = () => config.redirectOnError && config.redirectOnError.enabled;
 const redirectOnError = (err, req, res, next) => {
 
-	let url = config.redirectOnError.default_url;
+	let url = config.redirectOnError.default_url || '/error';
 
 	if (!err.code || !config.redirectOnError[err.err]) {
 		url = config.redirectOnError.internal_error || url;
 	}
 
-	url = config.redirectOnError[err.err];
+	url = config.redirectOnError[err.err] || url;
 
 	logger.info(`Redirecting to ${url} after error`, err, err.err);
-	res.redirect(url, next);
+	// set no-cache headers
+	res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
+	res.header('Expires', '-1');
+	res.header('Pragma', 'no-cache');
+	res.redirect(302, url, next);
 };
 
 module.exports = {
